2d515dc3920dbf88b1b3e7e742083d713de55c0d
Deniz Demirkan <denizdemirkan.drive@gmail.com>
2022-07-25 19:16:45 +0300

    Core Layer Improvements

---
+++ Course.TokenBasedAuth.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Course.TokenBasedAuth.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.TokenBasedAuth.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        Task<TEntity> GetByIdAsync(int Id);
+        Task<IQueryable<TEntity>> GetAllAsync();
+        Task CreateAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+
+    }
+}
