1e70482e7156d7cf1d614823d1f71422213c6cdd
Deniz Demirkan <denizdemirkan.drive@gmail.com>
2022-08-21 20:03:30 +0300

    DataAccess Layer Refactor

---
+++ Course.TokenBasedAuth.Repository/DbContexts/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Course.TokenBasedAuth.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.TokenBasedAuth.DataAccess.DbContexts
+{
+    public class AppDbContext : IdentityDbContext<User, IdentityRole, string>
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
